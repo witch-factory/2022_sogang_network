c374658e07e5d34b4437f6849c9d62e65f6225b3
witch-factory <soakdma37@gmail.com>
2022-04-15 21:18:54 +0900

    디코더 에러 감지 문제 해결중. 에러 발생시 글자 수 카운트가 제대로 안 됨
    ;
    '

--- crc_decoder.c
+++ crc_decoder.c
@@ -3,16 +3,16 @@
 #include <string.h>
 
 int char_xor(char a, char b){
-    if(a==b){return 0;}
-    else{return 1;}
+    if(a==b){return '0';}
+    else{return '1';}
 }
 
 int check_codeword(char* extended_dataword, int extended_dataword_size, char* generator, int generator_size){
-
+    //만약 codeword에 에러가 있었다면 remainder가 0이 아니다. 이 함수는 remainder를 반환한다.
     int remainder=0;
     int dataword_size=extended_dataword_size - generator_size + 1;
     int i,start,cur_xor_idx;
-    char* remainder_word=(char*)malloc(sizeof(char) * extended_dataword_size);
+    char* remainder_word=(char*)malloc(sizeof(char) * (extended_dataword_size+1));
     for(i=0;i<extended_dataword_size;i++){
         remainder_word[i]=extended_dataword[i];
     }
@@ -32,7 +32,8 @@ int check_codeword(char* extended_dataword, int extended_dataword_size, char* ge
     /*for(i=dataword_size;i<extended_dataword_size;i++){
         extended_dataword[i]=remainder_word[i];
     }*/
-    printf("%d\n", remainder);
+    printf("now remainder %s\n", remainder_word);
+    free(remainder_word);
     return remainder;
 }
 
@@ -42,22 +43,25 @@ int main(int argc, char** argv){
         printf("usage: ./crc_decoder input_file output_file result_file generator dataword_size\n");
         exit(1);
     }
-    char* input_file_name=argv[1];
-    char* output_file_name=argv[2];
-    char* result_file_name=argv[3];
-    char* generator=argv[4];
-    int generator_size=strlen(generator);
-    int dataword_size=strtol(argv[5], NULL, 10);
-    int codeword_size;
+    char* input_file_name, *output_file_name, *result_file_name, *generator;
+    int generator_size, dataword_size, codeword_size;
     FILE* input_file, *output_file, *result_file;
     char padding_size, cur_byte;
     int bit_index, i;
     int input_file_length=0;
     // 만약 dataword size가 4이면, 2개를 붙여서 디코딩해야 한다. 따라서 한 바이트가 다 채워졌는지를 나타내는 변수
-    //1이면 다 채워져서 다른 바이트를 시작해야 한다는 뜻
-    //0이면 1바이트의 반만 차 있는 상태
+    //4이면 1바이트의 반만 차 있다
+    //0이면 1바이트가 온전히 비어있다는 뜻
     int byte_filled=0;
     char byte_to_write=0;
+
+    input_file_name=argv[1];
+    output_file_name=argv[2];
+    result_file_name=argv[3];
+    generator=argv[4];
+    generator_size=strlen(generator);
+    dataword_size=strtol(argv[5], NULL, 10);
+    codeword_size=dataword_size + generator_size - 1;
 
     input_file=fopen(input_file_name, "r");
     if(input_file==NULL){
@@ -78,24 +82,25 @@ int main(int argc, char** argv){
         printf("dataword size must be 4 or 8.\n");
         exit(1);
     }
+    //read the padding size from the input file
     fread(&padding_size, 1, 1, input_file);
-    //printf("%d\n", padding_size);
 
+    input_file_length=0;
     //padding을 제외한 나머지 인코딩된 파일 길이 계산
     while((cur_byte= fgetc(input_file)) !=EOF){
         input_file_length++;
     }
-    //printf("%d\n", input_file_length);
+    printf("%d\n", input_file_length);
 
     fseek(input_file, 0, SEEK_SET);
     fread(&padding_size, 1, 1, input_file);
     //패딩 제거
     //bit_index는 이번에 읽을 비트 번지수
     bit_index=padding_size;
-    codeword_size=dataword_size + generator_size - 1;
 
     int codeword_num=0, error_codeword_num=0;
     char* current_codeword;
+    //read the first byte
     cur_byte= fgetc(input_file);
     byte_filled=0;
     while(cur_byte!=EOF){
@@ -144,17 +149,30 @@ int main(int argc, char** argv){
                     }
                 }
                 fprintf(output_file,"%c", byte_to_write);
-                printf("now decoded : %d\n", byte_to_write);
+                //printf("now decoded : %d\n", byte_to_write);
                 byte_filled=0;
                 byte_to_write=0;
-
             }
         }
-
+        else{
+            //dataword size is 8
+            for(i=0;i<dataword_size;i++){
+                if(current_codeword[i]=='1'){
+                    byte_to_write+=(1 << (dataword_size - i - 1));
+                }
+            }
+            fprintf(output_file,"%c", byte_to_write);
+            //printf("now decoded : %d\n", byte_to_write);
+            byte_to_write=0;
+        }
+        free(current_codeword);
     }
 
     printf("%d %d\n", codeword_num, error_codeword_num);
     fprintf(result_file, "%d %d\n", codeword_num, error_codeword_num);
 
+    fclose(input_file);
+    fclose(output_file);
+    fclose(result_file);
     return 0;
 }
--- crc_encoder.c
+++ crc_encoder.c
@@ -11,12 +11,12 @@ int char_xor(char a, char b){
 
 void codeword_extend(char* codeword, int codeword_size, int datanum, int dataword_size){
     int i;
-    //이진수 변환해서 앞쪽에 쓴다
+    //datanum은 이진수 변환해서 앞쪽에 쓴다
     for(i=dataword_size-1;i>=0;i--){
         codeword[i]=datanum%2 + '0';
         datanum=datanum/2;
     }
-    /* extension for crc. 뒤쪽에 쓴다 */
+    /* datanum의 이진수 변환 결과 뒤쪽에는 generator_size-1 만큼 0으로 채워 준다. */
     for(i=dataword_size;i<codeword_size;i++){
         codeword[i]='0';
     }
@@ -24,23 +24,27 @@ void codeword_extend(char* codeword, int codeword_size, int datanum, int datawor
 }
 
 void make_codeword(char* extended_dataword, int extended_dataword_size, char* generator, int generator_size){
+    //mod 2 division 을 진행해서 codeword로 바꿔 준다
     int dataword_size=extended_dataword_size - generator_size + 1;
     int i,start,cur_xor_idx;
-    char* remainder_word=(char*)malloc(sizeof(char) * extended_dataword_size);
+    //나머지를 계산해서 넣을 곳
+    char* remainder_word=(char*)malloc(sizeof(char) * (extended_dataword_size + 1));
     for(i=0;i<extended_dataword_size;i++){
         remainder_word[i]=extended_dataword[i];
     }
-
+    //generator로 나눈 나머지를 remainder_word에 담는다
     for(start=0;start<dataword_size;start++){
         if(remainder_word[start]=='0'){continue;} //최상위 비트가 1이 아니라면 넘어간다
         for(cur_xor_idx=start; cur_xor_idx<start+generator_size;cur_xor_idx++){
             remainder_word[cur_xor_idx] = char_xor(remainder_word[cur_xor_idx], generator[cur_xor_idx - start]);
         }
     }
-
     for(i=dataword_size;i<extended_dataword_size;i++){
         extended_dataword[i]=remainder_word[i];
     }
+    //끝에 널 문자.
+    extended_dataword[extended_dataword_size]='\0';
+    free(remainder_word);
     //printf("%s\n", extended_dataword);
 }
 
@@ -50,20 +54,22 @@ int main(int argc, char** argv) {
         printf("usage: ./crc_encoder input_file output_file generator dataword_size\n");
         exit(1);
     }
-    char* input_file_name=argv[1];
-    char* output_file_name=argv[2];
-    char* generator=argv[3];
-    int generator_size=strlen(generator);
-    int dataword_size=strtol(argv[4], NULL, 10);
-    int codeword_size;
+    char* input_file_name, *output_file_name;
+    char* generator;
+    int generator_size, dataword_size, codeword_size;
     FILE* input_file, *output_file;
-    //char input_file_name[]="/home/sunghyun/CLionProjects/network/temp.tx";
-    int cur;
-    int input_file_length, padding;
+    int cur, input_file_length, padding;
     int bit_index; //왼쪽을 0으로 할 때, 현재 바이트에서 몇 번째 비트를 현재 써야 하는가?
     char byte_to_write=0;
 
-    printf("%s\n", output_file_name);
+    input_file_name=argv[1];
+    output_file_name=argv[2];
+    generator=argv[3];
+    generator_size=strlen(generator);
+    dataword_size=strtol(argv[4], NULL, 10);
+    byte_to_write=0;
+
+    //file open check
     input_file=fopen(input_file_name, "r");
     if(input_file==NULL){
         printf("input file open error.\n");
@@ -78,7 +84,7 @@ int main(int argc, char** argv) {
         printf("dataword size must be 4 or 8.\n");
         exit(1);
     }
-    //coded word는 개당 dataword_size + generator_size - 1 만큼의 크기
+    //code word는 개당 dataword_size + generator_size - 1 만큼의 크기
     codeword_size=dataword_size + generator_size - 1;
     //입력 파일이 몇 글자인지
     input_file_length=0;
@@ -86,18 +92,21 @@ int main(int argc, char** argv) {
         input_file_length++;
     }
     //printf("input file size : %d\n", input_file_length);
+    //전체 인코딩 결과가 몇 byte가 될 것인가?
     padding=(input_file_length * (dataword_size == 4 ? 2 : 1)) * (codeword_size);
     padding=8 - (padding % 8);
     padding=padding % 8;
     //printf("%d padding\n", padding);
+    // write the number of the padding bits in output file
     fwrite(&padding, 1, 1, output_file);
 
     byte_to_write=0;
     bit_index=padding;
     //for(bit_index=0;bit_index<padding;bit_index++);
-    //padding만큼의 왼쪽 비트 0으로 만들기. padding은 무조건 0보다 작다.
+    //padding만큼의 왼쪽 비트 0으로 만들기. padding은 zero-padding이므로 무조건 0이다.
     //printf("byte to write : %d\n", byte_to_write);
 
+    //padding bit 계산을 했으므로 다시 파일을 처음부터 읽는다.
     fseek(input_file, 0, SEEK_SET);
 
     while((cur= fgetc(input_file)) !=EOF){
@@ -105,7 +114,6 @@ int main(int argc, char** argv) {
         int first_datanum, second_datanum;
         char* first_codeword, *second_codeword;
         int i,j;
-        //printf("%d %d\n", cur/16, cur%16);
         if(dataword_size==4){
             first_datanum=cur/16; //앞쪽 dataword
             second_datanum=cur%16;
@@ -121,7 +129,7 @@ int main(int argc, char** argv) {
             //이제 만들어진 codeword를 파일에 비트 단위로 써야 한다.
             for(i=0;i<codeword_size;i++){
                 if(bit_index==8){
-                    printf("%d\n",byte_to_write);
+                    //printf("%d\n",byte_to_write);
                     fwrite(&byte_to_write, 1, 1, output_file);
                     bit_index=0;
                     byte_to_write=0;
@@ -133,7 +141,7 @@ int main(int argc, char** argv) {
             }
             for(i=0;i<codeword_size;i++){
                 if(bit_index==8){
-                    printf("%d\n", byte_to_write);
+                    //printf("%d\n", byte_to_write);
                     fwrite(&byte_to_write, 1, 1, output_file);
                     bit_index=0;
                     byte_to_write=0;
@@ -144,11 +152,13 @@ int main(int argc, char** argv) {
                 bit_index++;
             }
             if(bit_index==8){
-                printf("%d\n", byte_to_write);
+                //printf("%d\n", byte_to_write);
                 fwrite(&byte_to_write, 1, 1, output_file);
                 bit_index=0;
                 byte_to_write=0;
             }
+            free(first_codeword);
+            free(second_codeword);
         }
         else{
             //dataword size is 8
@@ -161,7 +171,7 @@ int main(int argc, char** argv) {
 
             for(i=0;i<codeword_size;i++){
                 if(bit_index==8){
-                    printf("%d\n",byte_to_write);
+                    //printf("%d\n",byte_to_write);
                     fwrite(&byte_to_write, 1, 1, output_file);
                     bit_index=0;
                     byte_to_write=0;
@@ -171,8 +181,10 @@ int main(int argc, char** argv) {
                 }
                 bit_index++;
             }
+            free(first_codeword);
         }
     }
-
+    fclose(input_file);
+    fclose(output_file);
     return 0;
 }
